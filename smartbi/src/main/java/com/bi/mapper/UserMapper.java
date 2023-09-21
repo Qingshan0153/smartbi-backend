7fb334bb22298bc647183d68748e411c5e92ec2b
lishan1123 <ljl.1123@qq.c0m>
2023-09-21 20:51:32 +0800

    智能BI平台，初始化提交

---
+++ smartbi/src/main/java/com/bi/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.bi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author mendax
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
