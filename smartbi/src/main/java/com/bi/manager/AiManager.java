99400df26804a9fffeafdeebbce340cb5868a527
lishan1123 <ljl.1123@qq.c0m>
2023-09-24 12:16:57 +0800

    对接讯飞星火大模型,生成可视化图表信息

---
+++ smartbi/src/main/java/com/bi/manager/AiManager.java
@@ -0,0 +1,7 @@
+package com.bi.manager;/**
+ * 
+ * @version 2023/9/23 15:43
+ * @author mendax
+ */
+    public class AiManager {
+}
---
+++ smartbi/src/main/java/com/bi/model/vo/BiResponse.java
@@ -0,0 +1,7 @@
+package com.bi.model.vo;/**
+ * 
+ * @version 2023/9/23 16:30
+ * @author mendax
+ */
+    public class BiRsponse {
+}
